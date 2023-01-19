59678b61ba236dd3978e42a83ae7b8f8f96eafa3
eden <edensheiko@gmail.com>
2023-01-19 14:24:33 +0200

    * added updated Dockerfile for frontend * renamed the stream_lit.py to app.py there is 2 Dockerfiles that working togather the backend and the frontend

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,5 +1,12 @@
 import requests
 import streamlit as st
+import socket
+
+#getting ip
+hostname = socket.gethostname()
+ipaddress = socket.gethostbyname(hostname)
+ipaddress = "192.168.2.143"
+url = f"http://{ipaddress}/predict"
 
 st.title("Language Detection app 🚀")
 st.write("Language-Detection is a tool for inorder to "
@@ -11,7 +18,7 @@ text_input = st.text_area("Enter a text:")
 
 if st.button('Send API request'):
     payload = {"text": text_input}
-    response = requests.post('http://0.0.0.0/predict', json=payload)
+    response = requests.post(url, json=payload)
     if response.status_code == 200:
         data = response.json()
         st.write(data)
