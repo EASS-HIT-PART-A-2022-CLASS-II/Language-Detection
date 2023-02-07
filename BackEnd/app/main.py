8b8703ba537372784da62c60e643ffecb4573af2
eden <edensheiko@gmail.com>
2023-02-07 22:55:35 +0200

    added db_branch for testing

--- BackEnd/app/main.py
+++ BackEnd/app/main.py
@@ -1,4 +1,5 @@
 #imports
+import redis
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.ML_model.model import predict_pipeline
@@ -24,4 +25,11 @@ def home():
 @app.post("/predict", response_model=PredictionOut)
 def predict(payload: TextIn):
     language = predict_pipeline(payload.text)
+
+    # initialize Redis connection
+    r = redis.Redis(host='localhost', port=6379, db=0)
+
+    # increment the counter for the prediction language
+    r.incr(language)
+
     return {"language": language}
