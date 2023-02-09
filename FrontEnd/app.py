90271aa6278adae59bf8cf0919cb6b91b7867ba0
eden <edensheiko@gmail.com>
2023-02-09 19:03:57 +0200

    added db functionality with frontend api. #small bug in frontend code.

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import socket
 import psycopg2
 import json
-import subprocess
+import docker
 
 def get_host_ip():
     try:
@@ -15,10 +15,17 @@ def get_host_ip():
 
 url = f"http://{get_host_ip()}/predict"
 
+#code
+client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
+container = client.containers.get("some-postgres")
+container_ip = container.attrs['NetworkSettings']['IPAddress']
+print("The IP address of the container is: ", container_ip)
+
 
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
-    host="172.17.0.2",
+    #host="172.17.0.2",
+    host=container_ip,
     port="5432",
     user="postgres",
     password="mysecretpassword",
