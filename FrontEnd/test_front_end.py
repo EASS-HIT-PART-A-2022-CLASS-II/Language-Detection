a48b4349f8f32549117ef42c9a7b068eb49a4c5d
eden <edensheiko@gmail.com>
2023-02-11 01:02:58 +0200

    added test for the api for the front end + backend.

---
+++ FrontEnd/test_front_end.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import psycopg2
+import json
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:mysecretpassword@localhost/postgres")
+
+def test_api_request():
+    url = "http://localhost/predict"
+
+    payload = {"text": "Hello, World"}
+    response = requests.post(url, json=payload)
+    assert response.status_code == 200
+
+    data = response.json()
+    assert "language" in data
+
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(DATABASE_URL)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT text, result FROM results ORDER BY id DESC LIMIT 1")
+    result = cursor.fetchone()
+    assert result is not None
+
+    text, result_str = result
+    result = json.loads(result_str)
+    assert text == "test"
+    assert result == data
+
+    conn.close()
